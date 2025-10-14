1467c3907989123109de84bd102ab8c4f6c848cd
alban bertolini <albanb@forestadmin.com>
2025-10-14 11:51:24 +0200

    fix: test clean

--- bomber/src/__tests__/helpers.ts
+++ bomber/src/__tests__/helpers.ts
@@ -3,6 +3,7 @@ import { mountAgentClient } from "../agent-setup";
 export const INCOME_COLLECTION = "Api__Income";
 export const BANK_ACCOUNT_COLLECTION = "Api__BankAccount";
 export const ORGANIZATION_COLLECTION = "Api__Organization";
+export const PRODUCT_COLLECTION = "Biller__Product";
 
 export type AgentClient = Awaited<ReturnType<typeof mountAgentClient>>;
 
@@ -10,6 +11,7 @@ export const clearCollections = async (clientAgent: AgentClient) => {
   const incomes = clientAgent.collection(INCOME_COLLECTION);
   const bankAccounts = clientAgent.collection(BANK_ACCOUNT_COLLECTION);
   const organizations = clientAgent.collection(ORGANIZATION_COLLECTION);
+  const products = clientAgent.collection(PRODUCT_COLLECTION);
 
   const allIncomes = await incomes.list();
   if (allIncomes.length > 0) {
@@ -17,14 +19,15 @@ export const clearCollections = async (clientAgent: AgentClient) => {
   }
   const allBankAccounts = await bankAccounts.list();
   if (allBankAccounts.length > 0) {
-    await bankAccounts.delete(
-      allBankAccounts.map((bank) => String(bank.id))
-    );
+    await bankAccounts.delete(allBankAccounts.map((bank) => String(bank.id)));
   }
   const allOrganizations = await organizations.list();
   if (allOrganizations.length > 0) {
-    await organizations.delete(
-      allOrganizations.map((org) => String(org.id))
-    );
+    await organizations.delete(allOrganizations.map((org) => String(org.id)));
   }
-}
+
+  const allProducts = await products.list();
+  if (allProducts.length > 0) {
+    await products.delete(allProducts.map((product) => String(product.id)));
+  }
+};
